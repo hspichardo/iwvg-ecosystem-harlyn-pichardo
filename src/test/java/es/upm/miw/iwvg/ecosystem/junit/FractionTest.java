cd768f50a086696bf50e32bda0a35aa45186093f
hspichardo <hspichardo@gmail.com>
2019-09-22 02:17:51 +0200

    agregando mejora 1 a clase Fraction issue #14

--- src/test/java/es/upm/miw/iwvg/ecosystem/junit/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg/ecosystem/junit/FractionTest.java
@@ -27,4 +27,13 @@ public class FractionTest {
     void testGetDenominador(){
         assertEquals(2,fraction.getDenominator());
     }
+
+    @Test
+    void testIsPropia(){
+        assertEquals(false,fraction.isPropia());
+    }
+    @Test
+    void testIsImPropia(){
+        assertEquals(true,fraction.isImpropia());
+    }
 }
